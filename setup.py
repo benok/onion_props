495f7b49e9573315e4acaa7e572b18e3a90dbbfb
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2018-02-21 01:59:18 -0500

    restored setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-def setup(
-        name='prop_parser',
-        version='0.2.0',
-        description='An elegant hierarchical properties parser',
-        long_description=long_desc,
-        url='',
-        author='Conrad Heidebrecht',
-        author_email='',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'License :: GPL 3.0',
-            'Programming Language :: 2',
-            'Programming Language :: 2.7',
-            'Programming Language :: 3',
-        ],
-        keywords='properties java parser',
-        py_modules=['prop_parser'],
-        install_requires=['collections', 'datetime', 'os', 'time'],
-
+setup(
+    name='prop_parser',
+    version='0.2.0',
+    description='An elegant hierarchical properties parser',
+    long_description=long_desc,
+    url='',
+    author='Conrad Heidebrecht',
+    author_email='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: GPL 3.0',
+        'Programming Language :: 2',
+        'Programming Language :: 2.7',
+        'Programming Language :: 3',
+    ],
+    keywords='properties java parser',
+    py_modules=['prop_parser'],
+    install_requires=['collections', 'datetime', 'os', 'time'],
+)
 
